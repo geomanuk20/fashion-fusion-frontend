7f5546a3a3642ccc2f714a22958af7bc221568c3
geomanuk20 <geomanuk20@gmail.com>
2025-01-21 23:39:07 +0530

    first

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),pluginRewriteAll()],
+  plugins: [react()],
   build: { outDir: 'dist', },
 })
